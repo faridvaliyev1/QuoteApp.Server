154a32212eb42ae31612569cb5382f2155e8c916
faridval <farid.valiyev@ut.ee>
2021-01-27 01:09:34 +0200

    Adding Azure Cognitive services

--- EmotionApi/Features/Quotes/QuoteController.cs
+++ EmotionApi/Features/Quotes/QuoteController.cs
@@ -1,9 +1,71 @@
 ﻿using Catstagram.Server.Features;
+using EmotionApi.Features.Quotes.Models;
+using EmotionApi.Features.Quotes.Services;
+using EmotionApi.Functions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace EmotionApi.Features.Quotes
 {
+    [Authorize]
     public class QuoteController : ApiController
     {
-       
+        private readonly IQuoteService _quoteService;
+        private static IWebHostEnvironment _hostEnvironment;
+        
+        public QuoteController(IQuoteService quoteService,IWebHostEnvironment hostEnvironment)
+        {
+            _quoteService = quoteService;
+            _hostEnvironment = hostEnvironment;
+        }
+                  
+        public class FileUploadModel
+        {
+           public QuoteRequestModel model { get; set; }
+
+           public IFormFile file { get; set; }
+        }
+
+        [HttpPost]
+        [Route(nameof(Quote))]
+        public async Task<ActionResult<QuoteResponseModel>> Quote(FileUploadModel model)
+        {
+           if(model.file.Length>0)
+            {
+                try
+                {
+                    if(!Directory.Exists(_hostEnvironment.WebRootPath+"\\uploads"))
+                    {
+                        Directory.CreateDirectory(_hostEnvironment.WebRootPath + "\\upload");
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_hostEnvironment.WebRootPath + "\\uploads\\" + model.file.FileName))
+                    {
+                       await model.file.CopyToAsync(fileStream);
+                       fileStream.Flush();
+                        
+                    }
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+
+
+                var path = _hostEnvironment.WebRootPath + "\\uploads\\" + model.file.FileName;
+
+                string emotion =await HelpFunctions.detectEmotion(path);
+
+
+            }
+           else
+            {
+                return BadRequest();
+            }
+        }
+
     }
 }
---
+++ EmotionApi/Functions/HelpFunctions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EmotionApi.Functions
+{
+    public static class HelpFunctions
+    {
+       public static async Task<string> detectEmotion(string filepath)
+        {
+            List<FaceAttributeType> faces = new List<FaceAttributeType>
+            {
+                FaceAttributeType.Emotion
+            };
+            using (var client = new FaceClient(
+                new ApiKeyServiceClientCredentials("e579ceeb713f4d058f29a75b7ec3a21c"),
+                new System.Net.Http.DelegatingHandler[] { }))
+            {
+                client.Endpoint = "https://emotiondetection001.cognitiveservices.azure.com/";
+
+                using (var filestream = File.OpenRead(filepath))
+                {
+                    var detectionResult = await client.Face.DetectWithStreamAsync(filestream, returnFaceId: true, returnFaceAttributes: faces, returnFaceLandmarks: true);
+                    foreach (var face in detectionResult)
+                    {
+                        
+                        var highestEmotion = getEmotion(face.FaceAttributes.Emotion);
+                        return highestEmotion.Emotion;
+                    }
+                }
+            }
+            return "";
+        }
+
+       public static (string Emotion, double Value) getEmotion(Emotion emotion)
+        {
+            var emotionProperties = emotion.GetType().GetProperties();
+            (string Emotion, double Value) highestEmotion = ("Anger", emotion.Anger);
+            foreach (var e in emotionProperties)
+            {
+                if (((double)e.GetValue(emotion, null)) > highestEmotion.Value)
+                {
+                    highestEmotion.Emotion = e.Name;
+                    highestEmotion.Value = (double)e.GetValue(emotion, null);
+                }
+            }
+            return highestEmotion;
+        }
+    }
+}
